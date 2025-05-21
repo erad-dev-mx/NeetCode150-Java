1881b191888b1b364a4b85383df5e37b11a3dd8a
erad-dev-mx <erick_rr1@outlook.com>
2025-05-20 19:11:58 -0600

    LeetCode 347

---
+++ src/LeetCode357/Solution.java
@@ -0,0 +1,44 @@
+package LeetCode357;
+
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>(Comparator.comparingInt(count::get));
+
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = heap.poll();
+        }
+
+        return ans;
+    }
+}
+
+// Test class
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] result = solution.topKFrequent(nums, k);
+        System.out.println(Arrays.toString(result)); // Output: [1, 2]
+    }
+}
