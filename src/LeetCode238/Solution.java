3a9ecfb675a212c22dec258b2bb092029d949ea3
erad-dev-mx <erick_rr1@outlook.com>
2025-05-26 19:15:45 -0600

    Comments at LeetCode238 implementation

--- src/LeetCode238/Solution.java
+++ src/LeetCode238/Solution.java
@@ -2,6 +2,15 @@ package LeetCode238;
 
 import java.util.Arrays;
 
+/**
+ * Solution for LeetCode 238
+ *
+ * @see <a href="https://leetcode.com/problems/product-of-array-except-self/">LeetCode 238</a>
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ * This approach a two-pass algorithm to calculate the product of all elements except the current one.
+ */
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
@@ -21,5 +30,20 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums1))); // Expected output: [24, 12, 8, 6]
+
+        int[] nums2 = {5, 6, 7};
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums2))); // Expected output: [42, 35, 30]
+
+        int[] nums3 = {1};
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums3))); // Expected output: [1]
     }
 }
