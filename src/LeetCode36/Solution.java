1f4ae10fdc36be246752ccf82f5f9f29ab742044
erad-dev-mx <erick_rr1@outlook.com>
2025-05-28 13:16:19 -0600

    LeetCode 36 solution

---
+++ src/LeetCode36/Solution.java
@@ -0,0 +1,48 @@
+package LeetCode36;
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int r = 0; r < 9; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char val = board[r][c];
+
+                if (val == '.') {
+                    continue;
+                }
+
+                // Checking row
+                if (rows[r].contains(val)) {
+                    return false;
+                }
+                rows[r].add(val);
+
+                // Checking column
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                // Checking box
+                int boxNumber = (r / 3) * 3 + c / 3;
+                if (boxes[boxNumber].contains(val)) {
+                    return false;
+                }
+                boxes[boxNumber].add(val);
+            }
+        }
+
+        return true;
+    }
+}
