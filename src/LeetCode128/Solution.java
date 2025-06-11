4fe25b8e309f03d0514d61580ca66ef900de8b5d
erad-dev-mx <erick_rr1@outlook.com>
2025-06-10 19:35:03 -0600

    LeetCode 128 test class

--- src/LeetCode128/Solution.java
+++ src/LeetCode128/Solution.java
@@ -31,3 +31,13 @@ class Solution {
         return longestSub;
     }
 }
+
+// Test class
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = solution.longestConsecutive(nums);
+        System.out.println(result); // Output: 4
+    }
+}
