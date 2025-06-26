82f8a56917f59cbb5ed49aa88430d8d7606e196a
erad-dev-mx <erick_rr1@outlook.com>
2025-06-25 19:35:44 -0600

    LeetCode 239 solution

---
+++ src/LeetCode239/Solution.java
@@ -0,0 +1,38 @@
+package LeetCode239;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            // Removing indices that are out of the current window
+            while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+                deque.poll();
+            }
+
+            // Removing indices whose corresponding values are less than nums[i]
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            // Adding the current index to th deque
+            deque.offer(i);
+
+            // Adding the maximum element of the current window to the result
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peek()];
+            }
+        }
+
+        return result;
+    }
+}
