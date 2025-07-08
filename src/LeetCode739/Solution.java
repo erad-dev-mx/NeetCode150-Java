be562972236f70c4cce683bc776cd5bd0e814fcd
erad-dev-mx <erick_rr1@outlook.com>
2025-07-07 19:38:05 -0600

    LeetCode 739 test class

--- src/LeetCode739/Solution.java
+++ src/LeetCode739/Solution.java
@@ -27,3 +27,42 @@ class Solution {
         return answer;
     }
 }
+
+// Test class
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] temps1 = {73, 74, 75, 71, 69, 72, 76, 73};
+        printResult(temps1, solution.dailyTemperatures(temps1));
+
+        int[] temps2 = {30, 40, 50, 60};
+        printResult(temps2, solution.dailyTemperatures(temps2));
+
+        int[] temps3 = {30, 60, 90};
+        printResult(temps3, solution.dailyTemperatures(temps3));
+
+        int[] temps4 = {90, 80, 70, 60, 50};
+        printResult(temps4, solution.dailyTemperatures(temps4));
+
+        int[] temps5 = {70, 71, 70, 71, 70};
+        printResult(temps5, solution.dailyTemperatures(temps5));
+    }
+
+    private static void printResult(int[] input, int[] output) {
+        System.out.print("Input:  ");
+        printArray(input);
+        System.out.print("Output: ");
+        printArray(output);
+        System.out.println();
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
