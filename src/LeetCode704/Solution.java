b30e55eb852c37157626b5ae472f37f232e2ea20
erad-dev-mx <erick_rr1@outlook.com>
2025-07-11 22:33:43 -0600

    LeetCode 704 renaming variables

--- src/LeetCode704/Solution.java
+++ src/LeetCode704/Solution.java
@@ -10,18 +10,18 @@ package LeetCode704;
 
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-        while (start <= end) {
-            int middle = start + (end - start) / 2;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
 
             if (nums[middle] == target) {
                 return middle;
             } else if (nums[middle] > target) {
-                end = middle - 1;
+                right = middle - 1;
             } else {
-                start = middle + 1;
+                left = middle + 1;
             }
         }
 
