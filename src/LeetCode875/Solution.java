9a74c01d2c13498beb95fa44207005ec8962021b
erad-dev-mx <erick_rr1@outlook.com>
2025-07-14 19:35:38 -0600

    LeetCode 875 test class

--- src/LeetCode875/Solution.java
+++ src/LeetCode875/Solution.java
@@ -36,3 +36,25 @@ class Solution {
         return hours <= h;
     }
 }
+
+// Test class
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] piles1 = {3, 6, 7, 11};
+        System.out.println("Min speed: " + solution.minEatingSpeed(piles1, 8));
+
+        int[] piles2 = {30, 11, 23, 4, 20};
+        System.out.println("Min speed: " + solution.minEatingSpeed(piles2, 5));
+
+        int[] piles3 = {30, 11, 23, 4, 20};
+        System.out.println("Min speed: " + solution.minEatingSpeed(piles3, 6));
+
+        int[] piles4 = {312884470};
+        System.out.println("Min speed: " + solution.minEatingSpeed(piles4, 968709470));
+
+        int[] piles5 = {1, 1, 1, 1};
+        System.out.println("Min speed: " + solution.minEatingSpeed(piles5, 4));
+    }
+}
