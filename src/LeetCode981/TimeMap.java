14e9678e454e1f8c8688c1c1ad2a560e6a25a77c
erad-dev-mx <erick_rr1@outlook.com>
2025-07-16 19:52:46 -0600

    LeetCode 981 solution optimization

--- src/LeetCode981/TimeMap.java
+++ src/LeetCode981/TimeMap.java
@@ -18,16 +18,14 @@ class TimeMap {
     public TimeMap() {
         map = new HashMap<>();
     }
-    
+
     public void set(String key, String value, int timestamp) {
         map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
     }
-    
+
     public String get(String key, int timestamp) {
         TreeMap<Integer, String> treeMap = map.get(key);
-        if (treeMap == null) {
-            return "";
-        }
+        if (treeMap == null) return "";
         Map.Entry<Integer, String> entry = treeMap.floorEntry(timestamp);
         return entry == null ? "" : entry.getValue();
     }
