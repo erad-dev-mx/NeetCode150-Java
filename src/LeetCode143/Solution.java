dab199c179b3de7af97de9d83f02ba9eeb1116c1
erad-dev-mx <erick_rr1@outlook.com>
2025-07-22 19:22:47 -0600

    LeetCode 143 solution

--- src/LeetCode143/Solution.java
+++ src/LeetCode143/Solution.java
@@ -3,6 +3,41 @@ package LeetCode143;
 
 class Solution {
     public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        ListNode temp;
+
+        while (curr != null) {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second.next != null) {
+            temp = first.next;
+            first.next = second;
+            first = temp;
+
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
     }
 }
 
