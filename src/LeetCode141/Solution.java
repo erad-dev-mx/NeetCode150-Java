a9a3892a6c3f566ad5558d40dfacd82339950e57
erad-dev-mx <erick_rr1@outlook.com>
2025-07-29 19:20:31 -0600

    LeetCode 141 init

---
+++ src/LeetCode141/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode141;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
