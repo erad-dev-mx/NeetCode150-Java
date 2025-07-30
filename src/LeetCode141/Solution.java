570483110a95c82af684c5fbd141a249f9b307f6
erad-dev-mx <erick_rr1@outlook.com>
2025-07-29 19:24:22 -0600

    LeetCode 141 solution

--- src/LeetCode141/Solution.java
+++ src/LeetCode141/Solution.java
@@ -2,7 +2,27 @@ package LeetCode141;
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
 
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != null || fast != null) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            if (fast == slow) {
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
     }
 }
 
