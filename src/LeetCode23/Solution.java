3f377215d2e734ce0529948e3f8b147aa04aa60c
erad-dev-mx <erick_rr1@outlook.com>
2025-08-06 18:21:52 -0600

    LeetCode 23 solution

--- src/LeetCode23/Solution.java
+++ src/LeetCode23/Solution.java
@@ -1,8 +1,29 @@
 package LeetCode23;
 
+import java.util.PriorityQueue;
+
+
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
+        for (ListNode list: lists) {
+            while (list != null) {
+                minHeap.add(list.val);
+                list = list.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(1);
+        ListNode merge = dummy;
+
+        while (!minHeap.isEmpty()) {
+            merge.next = new ListNode(minHeap.remove());
+            merge = merge.next;
+        }
+
+        return dummy.next;
     }
 }
 
