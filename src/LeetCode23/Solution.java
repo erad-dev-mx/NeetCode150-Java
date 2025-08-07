737093d3f2c9e51835740cafb9b00e05242c7b01
erad-dev-mx <erick_rr1@outlook.com>
2025-08-06 18:22:49 -0600

    LeetCode 23 test class

--- src/LeetCode23/Solution.java
+++ src/LeetCode23/Solution.java
@@ -59,3 +59,21 @@ class ListNode {
         this.next = next;
     }
 }
+
+// Test class
+class TestMergeKLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode l3 = new ListNode(2, new ListNode(6));
+
+        ListNode[] lists = new ListNode[]{l1, l2, l3};
+        Solution solution = new Solution();
+
+        ListNode result = solution.mergeKLists(lists);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
