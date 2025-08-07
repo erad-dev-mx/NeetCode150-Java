4bdacfbaedfdd88567f48a38af130da617f77efb
erad-dev-mx <erick_rr1@outlook.com>
2025-08-06 18:09:34 -0600

    LeetCode 23 init

---
+++ src/LeetCode23/Solution.java
@@ -0,0 +1,34 @@
+package LeetCode23;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
