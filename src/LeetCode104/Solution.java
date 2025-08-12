676408e4ca2e46c208028f310ccc6949a393ba4b
erad-dev-mx <erick_rr1@outlook.com>
2025-08-11 19:49:06 -0600

    LeetCode 104 solution

--- src/LeetCode104/Solution.java
+++ src/LeetCode104/Solution.java
@@ -2,7 +2,14 @@ package LeetCode104;
 
 class Solution {
     public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int maxLeft = maxDepth(root.left);
+            int maxRight = maxDepth(root.right);
 
+            return Math.max(maxLeft, maxRight) + 1;
+        }
     }
 }
 
