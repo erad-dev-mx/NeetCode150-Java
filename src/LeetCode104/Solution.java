78dbe92bee0976227537dd8179eef6e88cc3b1ae
erad-dev-mx <erick_rr1@outlook.com>
2025-08-11 19:49:22 -0600

    LeetCode 104 test class

--- src/LeetCode104/Solution.java
+++ src/LeetCode104/Solution.java
@@ -31,3 +31,25 @@ class TreeNode {
         this.right = right;
     }
 }
+
+// Test class
+class SolutionTest {
+    public static void main(String[] args) {
+        // Crear el árbol de ejemplo:
+        //       3
+        //      / \
+        //     9  20
+        //       /  \
+        //      15   7
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        int depth = solution.maxDepth(root);
+
+        System.out.println("Max Depth: " + depth);
+    }
+}
