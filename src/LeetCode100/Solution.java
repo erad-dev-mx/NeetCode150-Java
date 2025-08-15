09aa14c3aae809797c9f927cea8e2dee57bee427
erad-dev-mx <erick_rr1@outlook.com>
2025-08-14 20:51:50 -0600

    LeetCode 100 solution

--- src/LeetCode100/Solution.java
+++ src/LeetCode100/Solution.java
@@ -2,7 +2,13 @@ package LeetCode100;
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
 
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 
