c3b5175d545394f60027ead32334a744d0f665ba
erad-dev-mx <erick_rr1@outlook.com>
2025-08-14 20:47:15 -0600

    LeetCode 100 definition for binary tree

--- src/LeetCode100/Solution.java
+++ src/LeetCode100/Solution.java
@@ -2,6 +2,28 @@ package LeetCode100;
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
