0b9f915775920bbdb3a7bfd359e2cf227978b35e
erad-dev-mx <erick_rr1@outlook.com>
2025-08-15 19:44:51 -0600

    LeetCode 199 solution fix

--- src/LeetCode199/Solution.java
+++ src/LeetCode199/Solution.java
@@ -28,25 +28,26 @@ class Solution {
         }
         return result;
     }
+}
 
-    /**
-     * Definition for a binary tree node.
-     */
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    TreeNode() {
     }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
