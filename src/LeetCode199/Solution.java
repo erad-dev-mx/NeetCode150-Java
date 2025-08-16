295d92a6aa4237bb2d3c012e0e1a9a333d46c48d
erad-dev-mx <erick_rr1@outlook.com>
2025-08-15 19:44:22 -0600

    LeetCode 199 solution

---
+++ src/LeetCode199/Solution.java
@@ -0,0 +1,52 @@
+package LeetCode199;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // Add the last node's value of each level to result
+                if (i == levelSize - 1) result.add(currentNode.val);
+
+                // Add child nodes to the queue for next level
+                if (currentNode.left != null) queue.add(currentNode.left);
+                if (currentNode.right != null) queue.add(currentNode.right);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
