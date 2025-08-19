829794807d0433e4972d14d03b39d113838c930e
erad-dev-mx <erick_rr1@outlook.com>
2025-08-18 20:23:21 -0600

    Review of LeetCode 217 Contains Duplicate

---
+++ src/LeetCode217/Review/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode217.Review;
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+
+        for (int num : nums) {
+            if (seenNumbers.contains(num)) {
+                return true;
+            }
+            seenNumbers.add(num);
+        }
+
+        return false;
+    }
+}
