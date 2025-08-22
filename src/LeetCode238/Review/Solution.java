b3140143224949ebb11ba06a301ce3cc594f343e
erad-dev-mx <erick_rr1@outlook.com>
2025-08-21 20:28:07 -0600

    Review of LeetCode 238 thoughts

--- src/LeetCode238/Review/Solution.java
+++ src/LeetCode238/Review/Solution.java
@@ -23,3 +23,9 @@ class Solution {
         return result;
     }
 }
+
+// My thoughts: This problem it is a bit difficult for me, so the first thing is to create a new array with the length
+// of nums array. After that, we will fill the array with 1 in all positions. It is time to create two variables: pre
+// and pos. Now we run through all array from left to right multiplying the previous values of current. Finally, we will
+// run from right to left, multiplying the values that we have from the first run by pos variable, and we update pos
+// similar to the first run.
