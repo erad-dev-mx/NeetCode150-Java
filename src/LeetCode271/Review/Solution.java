667e15b592bbdef1435d5be3a3861abf718da035
erad-dev-mx <erick_rr1@outlook.com>
2025-08-27 20:07:11 -0600

    Review of LeetCode 271 solution

--- src/LeetCode271/Review/Solution.java
+++ src/LeetCode271/Review/Solution.java
@@ -1,13 +1,31 @@
 package LeetCode271.Review;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
+        if (strs.size() == 0) return Character.toString((char) 258);
+
+        String separator = Character.toString((char) 257);
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s);
+            sb.append(separator);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
-    public String encode(String strs) {
+    public List<String> encode(String s) {
+        if (s.equals(Character.toString((char) 258))) return new ArrayList();
+
+        String separator = Character.toString((char) 257);
+
+        return Arrays.asList(s.split(separator, -1));
     }
 }
