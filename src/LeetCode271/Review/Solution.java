d11da7694aaaf589066b3df4bc350e9f3316efb7
erad-dev-mx <erick_rr1@outlook.com>
2025-08-27 19:53:23 -0600

    Review of LeetCode 271 init

---
+++ src/LeetCode271/Review/Solution.java
@@ -0,0 +1,8 @@
+package LeetCode271.Review;
+
+import java.util.List;
+
+public class Solution {
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+    }
