c82389647d1aaacc59bf4d08e43f3257f87650c7
erad-dev-mx <erick_rr1@outlook.com>
2025-09-01 20:19:57 -0600

    Review of LeetCode 128 thoughts

--- src/LeetCode128/Review/Solution.java
+++ src/LeetCode128/Review/Solution.java
@@ -28,3 +28,11 @@ class Solution {
         return longestSub;
     }
 }
+
+// My thoughts: This problem can be solved using Binary Search and then with the help of some variables we can reach
+// The longest consecutive in the array but that solution is using O (N Log (n)) time because of the BS and the effort
+// to find the longest consecutive. A better approach is using a HashSet we will run a loop to fill the Data Structure
+// with the values (and eliminating duplicates). Having the numSet we can see if the value in the loop is part of a
+// sequence and if is not we will use a couple of variables to track the current number and the current longest
+// subsequence. At the end, we will just validate which value is greater, the current value of longest subsequence or
+// the temporary that we are tracking inside our loop.
