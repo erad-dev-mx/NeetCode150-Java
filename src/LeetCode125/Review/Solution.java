2f72b92d836b0fd57169b87125d655fd0076fa95
erad-dev-mx <erick_rr1@outlook.com>
2025-09-02 21:59:15 -0600

    Review of LeetCode 125 Solution

--- src/LeetCode125/Review/Solution.java
+++ src/LeetCode125/Review/Solution.java
@@ -2,6 +2,25 @@ package LeetCode125.Review;
 
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
