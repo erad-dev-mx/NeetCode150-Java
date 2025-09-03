970b2ef94367827c3261d4f4650a535378ea2a55
erad-dev-mx <erick_rr1@outlook.com>
2025-09-02 22:01:55 -0600

    Review of LeetCode 125 thoughts

--- src/LeetCode125/Review/Solution.java
+++ src/LeetCode125/Review/Solution.java
@@ -24,3 +24,8 @@ class Solution {
         return true;
     }
 }
+
+// My thoughts: To solve this problem the better approach is using a two-pointer technique algorithm. With left and
+// right pointers we will track the string from the beginning to the end, for left we will increase our pointer,
+// otherwise we will decrease our right pointer, the only complexity is validating if current value is a valid
+// character because we can receive symbols like commas, points, etc.
