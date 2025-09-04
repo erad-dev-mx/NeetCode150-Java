2563f2dae097b159b355959e6cdfbe0995e5b5e1
erad-dev-mx <erick_rr1@outlook.com>
2025-09-03 20:16:32 -0600

    Review of LeetCode 167 solution

--- src/LeetCode167/Review/Solution.java
+++ src/LeetCode167/Review/Solution.java
@@ -2,6 +2,19 @@ package LeetCode167.Review;
 
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                return new int[]{left + 1, right + 1};
+            }
+        }
+
+        return null;
     }
 }
