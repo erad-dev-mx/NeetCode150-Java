073966c8a57e8b8a2de6666d83dbc28865a2f2a8
erad-dev-mx <erick_rr1@outlook.com>
2025-09-17 19:43:06 -0600

    Review of LeetCode 3 answer

--- src/LeetCode3/Review/Solution.java
+++ src/LeetCode3/Review/Solution.java
@@ -1,7 +1,29 @@
 package LeetCode3.Review;
 
+import java.util.HashSet;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        if (s == null || s.length() == 0) return 0;
+
+        if (s.length() == 1) return 1;
+
+        int left = 0;
+        int right = 0;
+        int answer = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            answer = Math.max(answer, right - left + 1);
+            right++;
+        }
+
+        return answer;
     }
 }
