33f5d98e59da0844dd46b72d201b1687571ee1b7
erad-dev-mx <erick_rr1@outlook.com>
2025-09-18 20:21:24 -0600

    Review of LeetCode 424 solution

--- src/LeetCode424/Review/Solution.java
+++ src/LeetCode424/Review/Solution.java
@@ -2,6 +2,20 @@ package LeetCode424.Review;
 
 class Solution {
     public int characterReplacement(String s, int k) {
-        
+        int[] occurrence = new int[26];
+        int left = 0;
+        int ans = 0;
+        int maxOccurrence = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            maxOccurrence = Math.max(maxOccurrence, ++occurrence[s.charAt(i) - 'A']);
+            if (i - left + 1 - maxOccurrence > k) {
+                occurrence[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, i - left + 1);
+        }
+
+        return ans;
     }
 }
