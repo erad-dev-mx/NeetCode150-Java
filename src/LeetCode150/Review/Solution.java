2ae23482407a7974433971cacde3cc98982ba19c
erad-dev-mx <erick_rr1@outlook.com>
2025-10-06 19:40:32 -0600

    [Solution] Review of LeetCode 150

--- src/LeetCode150/Review/Solution.java
+++ src/LeetCode150/Review/Solution.java
@@ -1,7 +1,36 @@
 package LeetCode150.Review;
 
+import java.util.Stack;
+
 class Solution {
     public int evalRPN(String[] tokens) {
-        
+        Stack<Integer> stack = new Stack<>();
+
+        for (String t : tokens) {
+            if (isOperator(t)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperator(t, a, b);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(t));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperator(String operator, int a, int b) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> a / b;
+            default -> throw new IllegalArgumentException("Invalid operator");
+        };
     }
 }
