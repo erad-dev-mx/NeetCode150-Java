188f57f029017bdcc88a545a4426e60b12d86219
erad-dev-mx <erick_rr1@outlook.com>
2025-10-07 22:29:46 -0600

    [Solution] Review of LeetCode 22

--- src/LeetCode22/Review/Solution.java
+++ src/LeetCode22/Review/Solution.java
@@ -1,7 +1,32 @@
 package LeetCode22.Review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<String> generateParenthesis(int n) {
-        
+        List<String> ans = new ArrayList<>();
+
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            ans.add(current.toString());
+            return;
+        }
+
+        if (open < max) {
+            current.append("(");
+            backtrack(ans, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1);
+        }
+        if (close < open) {
+            current.append(")");
+            backtrack(ans, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1);
+        }
     }
 }
