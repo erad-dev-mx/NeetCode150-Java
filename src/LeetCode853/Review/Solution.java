5fdd243881c74096d9417ef8aeff9f922fa16a07
erad-dev-mx <erick_rr1@outlook.com>
2025-10-09 19:26:11 -0600

    [Quick Note] Review of LeetCode 853

--- src/LeetCode853/Review/Solution.java
+++ src/LeetCode853/Review/Solution.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        double[][] cars = new double[n][2];
+        double[][] cars = new double[n][2]; // Just to not forgot: n is number of rows and 2 number of cols
 
         for (int i = 0; i < n; i++) {
             cars[i][0] = position[i];
