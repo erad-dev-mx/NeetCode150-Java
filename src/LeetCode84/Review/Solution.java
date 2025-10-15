3ee54e16878bfbbc254eda87a1cbcbb506d1412f
erad-dev-mx <erick_rr1@outlook.com>
2025-10-14 19:02:14 -0600

    [Fix] Review of LeetCode 84

--- src/LeetCode84/Review/Solution.java
+++ src/LeetCode84/Review/Solution.java
@@ -7,7 +7,7 @@ class Solution {
         int maxArea = 0;
         Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < heights.length; i++) {
+        for (int i = 0; i <= heights.length; i++) {
             int currentHeight = (i == heights.length) ? 0 : heights[i];
 
             while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
