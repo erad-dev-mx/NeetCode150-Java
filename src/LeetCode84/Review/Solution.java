ceac326a8553fb25cf014a532ac415805782e27a
erad-dev-mx <erick_rr1@outlook.com>
2025-10-14 18:54:01 -0600

    [Solution] Review of LeetCode 84

--- src/LeetCode84/Review/Solution.java
+++ src/LeetCode84/Review/Solution.java
@@ -1,7 +1,23 @@
 package LeetCode84.Review;
 
+import java.util.Stack;
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < heights.length; i++) {
+            int currentHeight = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
     }
 }
