e795be6760e7e796d81f8f04922c5824a8445506
erad-dev-mx <erick_rr1@outlook.com>
2025-10-14 19:00:42 -0600

    [Thoughts] Review of LeetCode 84

--- src/LeetCode84/Review/Solution.java
+++ src/LeetCode84/Review/Solution.java
@@ -21,3 +21,11 @@ class Solution {
         return maxArea;
     }
 }
+
+// Thoughts: To solve the problem we will use a stack that will help us. We will push the first element to the stack, if
+// te next one is greater we will push that value in our stack but if it is less we will push the previous from the
+// stack, and we will put out new value. In teh stack we are going to add the index value to access to the position and
+// the value itself. When the heigh suddenly increase we will add the value to the stack because that means that the
+// previous one is still valid. Just until we found a lower value we will pop the values from stack or if we reach to
+// the end, also we are going to pop out the index value. Everytime we pop out a value we need to calculate the area and
+// keep it in maxArea (well, just if it is the greatest at that moment).
