82c5c6d2586e50c76c96bcb7ff648186c7eb7329
erad-dev-mx <erick_rr1@outlook.com>
2025-10-16 19:15:28 -0600

    [Thoughts] Review of LeetCode 704

--- src/LeetCode704/Review/Solution.java
+++ src/LeetCode704/Review/Solution.java
@@ -19,3 +19,9 @@ class Solution {
         return -1;
     }
 }
+
+// My thoughts: We can use binary search when we have a sort array. If we have to find a number inside the array, we can
+// use brute force iterating through the whole array until we found the target, with this approach the time complexity
+// is o(n) so a better way is to use binary search, basically we will compare target with the mid-element, if is greater
+// than the target, we will decrease the right pointer (end pointer), in other case, we will increase left pointer to
+// have a new beginning.
