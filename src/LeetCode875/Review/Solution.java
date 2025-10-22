5c7d66ec63bc9825168ddda823acedf5bbfbc202
erad-dev-mx <erick_rr1@outlook.com>
2025-10-21 19:30:59 -0600

    [Solution] Review of Leetcode 875

--- src/LeetCode875/Review/Solution.java
+++ src/LeetCode875/Review/Solution.java
@@ -1,5 +1,33 @@
 package LeetCode875.Review;
 
 class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1;
 
+        for (int pile : piles) {
+            right = Math.max(pile, right);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canFinish(piles, mid, h)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean canFinish(int[] piles, int speed, int h) {
+        int hours = 0;
+        for(int pile : piles) {
+            hours += (int) Math.ceil((double) pile / speed);
+        }
+
+        return hours <= h;
+    }
 }
