0e23c6454671b2ca74cee19b0bf930eff23977e7
erad-dev-mx <erick_rr1@outlook.com>
2025-10-22 19:26:33 -0600

    [Solution] Review of Leetcode 153

--- src/LeetCode153/Review/Solution.java
+++ src/LeetCode153/Review/Solution.java
@@ -2,6 +2,27 @@ package LeetCode153.Review;
 
 class Solution {
     public int findMin(int[] nums) {
-        
+        int left = 0;
+        int right = nums.length - 1;
+        int answer = nums[0];
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        while (left <= right) {
+            if (nums[left] < nums[right]) {
+                answer = Math.min(answer, nums[left]);
+            }
+            int mid = left + (right - left) / 2;
+            answer = Math.min(answer, nums[mid]);
+            if (nums[left] <= nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
     }
 }
