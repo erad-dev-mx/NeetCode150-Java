4eec98da5853053672e7f1520cc6649971558e11
erad-dev-mx <erick_rr1@outlook.com>
2025-11-25 20:20:17 -0600

    [Solution] Review of Leetcode 141

--- src/LeetCode141/Review/Solution.java
+++ src/LeetCode141/Review/Solution.java
@@ -13,7 +13,21 @@ package LeetCode141.Review;
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
 
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != null || fast != null) {
+            if (fast == null || fast.next == null) return false;
+
+            if (fast == slow) return true;
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
     }
 }
 
