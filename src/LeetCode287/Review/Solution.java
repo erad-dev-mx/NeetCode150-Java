dbabae7f07fc578ff5c0bff9ad54dbb92ce67c54
erad-dev-mx <erick_rr1@outlook.com>
2025-11-29 21:12:22 -0600

    [Thoughts] Review of Leetcode 287

--- src/LeetCode287/Review/Solution.java
+++ src/LeetCode287/Review/Solution.java
@@ -21,3 +21,8 @@ class Solution {
         return slow;
     }
 }
+
+// Thoughts: To solve this problem we use the tortoise and hare algorithm, what that means? we will move slow pointer
+// one a time, and fast pointer twice a time. When they are equal that means we are in a cycle. So we will move slow to
+// 0 position, and we will move both the pointers one a time until they find themselves. This is the way to solve the
+// problem avoiding an extra data structure and without modifying the integer arrays.
