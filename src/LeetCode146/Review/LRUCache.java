ebe3a8594a1a21ccea304150bf63c8c7e04d3a1d
erad-dev-mx <erick_rr1@outlook.com>
2025-12-01 18:59:31 -0600

    [Init] Review of Leetcode 146

---
+++ src/LeetCode146/Review/LRUCache.java
@@ -0,0 +1,23 @@
+    package LeetCode146.Review;
+
+class LRUCache {
+
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
