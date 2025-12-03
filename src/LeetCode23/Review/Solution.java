aff8c378e9de04c2685b1a81de92606d7901765a
erad-dev-mx <erick_rr1@outlook.com>
2025-12-02 20:12:27 -0600

    [Thoughts] Review of Leetcode 23

--- src/LeetCode23/Review/Solution.java
+++ src/LeetCode23/Review/Solution.java
@@ -51,3 +51,8 @@ class ListNode {
         this.next = next;
     }
 }
+
+// My thoughts: To solve this problem there are different ways to do it. One is using three o k different pointers
+// (number of lists), and compare which is the smaller one and so on, but this will have a time complexity of o (n x k).
+// So another way is using a PriorityQueue that will help us to order all the LinkedList after that we will put the
+// values in a new ListNode and that will be our answer.
