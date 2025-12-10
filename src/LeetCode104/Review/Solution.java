bf758cdfefe874399789d5aba8ab6de75ee8ffe7
erad-dev-mx <erick_rr1@outlook.com>
2025-12-09 19:52:44 -0600

    [Solution] Review of Leetcode 104

--- src/LeetCode104/Review/Solution.java
+++ src/LeetCode104/Review/Solution.java
@@ -19,7 +19,14 @@ package LeetCode104.Review;
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftMax = maxDepth(root.left);
+            int rightMax = maxDepth(root.right);
 
+            return Math.max(leftMax, rightMax) + 1;
+        }
     }
 }
 
