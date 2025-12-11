9009d12afd8b3b7b3b57d3c72f6538f7a028ae23
erad-dev-mx <erick_rr1@outlook.com>
2025-12-10 19:39:25 -0600

    [Solution] Review of Leetcode 543

--- src/LeetCode543/Review/Solution.java
+++ src/LeetCode543/Review/Solution.java
@@ -16,8 +16,23 @@ package LeetCode543.Review;
  * }
  */
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
+    int maxDiameter = 0;
 
+    public int diameterOfBinaryTree(TreeNode root) {
+        getHeight(root);
+        return maxDiameter;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+
+        maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
+
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
 
