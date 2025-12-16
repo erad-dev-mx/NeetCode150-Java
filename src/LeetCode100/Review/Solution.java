23b1f7d45c60849b54a98b10ddbc911e438b5188
erad-dev-mx <erick_rr1@outlook.com>
2025-12-15 19:00:04 -0600

    [Solution] Review of Leetcode 100

--- src/LeetCode100/Review/Solution.java
+++ src/LeetCode100/Review/Solution.java
@@ -17,11 +17,16 @@ package LeetCode100.Review;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
 
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 
-public class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
