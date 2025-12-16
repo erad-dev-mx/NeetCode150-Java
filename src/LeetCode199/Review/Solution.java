ee2e1a8db484e093c5482b0f577b420ac75da0e5
erad-dev-mx <erick_rr1@outlook.com>
2025-12-15 19:37:31 -0600

    [Solution] Review of Leetcode 199

--- src/LeetCode199/Review/Solution.java
+++ src/LeetCode199/Review/Solution.java
@@ -1,6 +1,9 @@
 package LeetCode199.Review;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 // https://leetcode.com/problems/binary-tree-right-side-view/
 
@@ -21,7 +24,26 @@ import java.util.List;
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll(); // Remove first element (Queue - First In First Out)
+                // Add the last node's value of each level to the result list
+                if (i == levelSize - 1) result.add(currentNode.val);
+
+                // Add child nodes to the queue for the next level
+                if (currentNode.left != null) queue.add(currentNode.left);
+                if (currentNode.right != null) queue.add(currentNode.right);
+            }
+        }
+        return result;
     }
 }
 
