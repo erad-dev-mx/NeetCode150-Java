723fb6da0653e4c793fde3ca33f8c742c97c0855
erad-dev-mx <erick_rr1@outlook.com>
2025-12-16 19:21:12 -0600

    [Solution] Leetcode 1448

--- src/LeetCode1448/Solution.java
+++ src/LeetCode1448/Solution.java
@@ -19,7 +19,23 @@ package LeetCode1448;
  */
 class Solution {
     public int goodNodes(TreeNode root) {
+        return countGoodNodes(root, Integer.MIN_VALUE);
+    }
 
+    private int countGoodNodes(TreeNode node, int maxSoFar) {
+        if (node == null) return 0;
+
+        int count = 0;
+        // Check if the current node is a good node
+        if (node.val >= maxSoFar) {
+            count = 1; // Current node is a good node
+            maxSoFar = node.val; // Update the max value along the path
+        }
+
+        count += countGoodNodes(node.left, maxSoFar);
+        count += countGoodNodes(node.right, maxSoFar);
+
+        return count;
     }
 }
 
