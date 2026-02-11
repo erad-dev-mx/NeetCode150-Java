db261bf9804ec3f2eb304e8f429419f619bb3702
erad-dev-mx <erick_rr1@outlook.com>
2026-02-10 19:37:21 -0600

    [Review 2.0] Thought process Leetcode 121

--- src/LeetCode121/Review/ReviewV2.java
+++ src/LeetCode121/Review/ReviewV2.java
@@ -1,6 +1,11 @@
 package LeetCode121.Review;
 
 public class ReviewV2 {
+    // int min = prices[0], int profit = 0
+    // Then, run a loop in prices
+    // if (prices[i] < min) min = prices[i]
+    // profit = maxOf(profit, prices[i] - min);
+    // Time Complexity = o(n), Space Complexity = o(1)
     public int maxProfit(int[] prices) {
 
     }
