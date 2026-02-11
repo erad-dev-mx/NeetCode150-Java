fb7911f4319747b6f6edfed2cd0b3280a5a45bc1
erad-dev-mx <erick_rr1@outlook.com>
2026-02-10 19:37:41 -0600

    [Review 2.0] Code of Leetcode 121

--- src/LeetCode121/Review/ReviewV2.java
+++ src/LeetCode121/Review/ReviewV2.java
@@ -7,6 +7,15 @@ public class ReviewV2 {
     // profit = maxOf(profit, prices[i] - min);
     // Time Complexity = o(n), Space Complexity = o(1)
     public int maxProfit(int[] prices) {
+        int minValue = prices[0];
+        int profit = 0;
 
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minValue) minValue = prices[i];
+
+            profit = Math.max(profit, prices[i] - minValue);
+        }
+
+        return profit;
     }
 }
