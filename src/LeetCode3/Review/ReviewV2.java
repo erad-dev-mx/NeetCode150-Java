fb5913cc1faba4f43659ee1df6e110758422f18f
erad-dev-mx <erick_rr1@outlook.com>
2026-02-11 20:00:44 -0600

    [Review 2.0] Thoughts of Leetcode 3

--- src/LeetCode3/Review/ReviewV2.java
+++ src/LeetCode3/Review/ReviewV2.java
@@ -1,6 +1,12 @@
 package LeetCode3.Review;
 
 class ReviewV2 {
+    // HashSet, left = 0, right = 0, answer = 0
+    // abcabcbb
+    // while char at ith position is not present in set, then add value and ++ right pointer
+    // but if char at ith posirion is present in set, we remove char at left and increase this one
+    // abc => bca => cab => abc => bc
+    // We track which is the greater to return as answer
     public int lengthOfLongestSubstring(String s) {
 
     }
