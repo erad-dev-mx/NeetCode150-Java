1356e74113762b395d1048dfcf87c092bda5cb24
erad-dev-mx <erick_rr1@outlook.com>
2026-02-11 20:01:54 -0600

    [Review 2.0] Code of Leetcode 3

--- src/LeetCode3/Review/ReviewV2.java
+++ src/LeetCode3/Review/ReviewV2.java
@@ -8,6 +8,25 @@ class ReviewV2 {
     // abc => bca => cab => abc => bc
     // We track which is the greater to return as answer
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
 
+        int left = 0;
+        int right = 0;
+        int answer = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            answer = Math.max(answer, right - left + 1);
+            right++;
+        }
+
+        return answer;
     }
 }
