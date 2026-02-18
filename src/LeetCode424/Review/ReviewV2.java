bb4ba213d13716c0a2f1a2bac1022d3c42e3372b
erad-dev-mx <erick_rr1@outlook.com>
2026-02-17 19:34:49 -0600

    [Review 2.0] Code of Leetcode 424

--- src/LeetCode424/Review/ReviewV2.java
+++ src/LeetCode424/Review/ReviewV2.java
@@ -12,6 +12,20 @@ public class ReviewV2 {
     // left  = 0, right = 3, window = [AABA], m0 = 3 ---> left = 0, right = 4, window = [AABAB], m0 = 3
     // 5 - 3 = 2 then k < 2
     public int characterReplacement(String s, int k) {
+        int occurrence[] = new int[26];
+        int left = 0;
+        int answer = 0;
+        int maxOccurrence = 0;
 
+        for (int right = 0; right < s.length(); right++) {
+            maxOccurrence = Math.max(maxOccurrence, ++occurrence[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxOccurrence > k) {
+                occurrence[s.charAt(left) - 'A']--;
+                left++;
+            }
+            answer = Math.max(answer, right - left + 1);
+        }
+
+        return answer;
     }
 }
