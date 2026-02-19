136a59c05965929ded9526c951467d7813473aed
erad-dev-mx <erick_rr1@outlook.com>
2026-02-18 20:25:51 -0600

    [Review 2.0] Thought process of Leetcode 567

--- src/LeetCode567/Review/ReviewV2.java
+++ src/LeetCode567/Review/ReviewV2.java
@@ -1,6 +1,11 @@
 package LeetCode567.Review;
 
 public class ReviewV2 {
+    // if (s1.length > s2.length) return false
+    // s1Map & s2Map
+    // Input: s1 = "ab", s2 = "eidbaooo"
+    // s1Map: {a:1, b:1}; s2Map: {e:1, i:1} > s2Map: {i:1, d:1} > s2Map: {d:1, b:1} > s2Map: {b:1, a:1}
+    // true
     public boolean checkInclusion(String s1, String s2) {
 
     }
