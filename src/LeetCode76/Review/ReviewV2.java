7eaf1379c636ec975ff1ad240b281785ed0d474b
erad-dev-mx <erick_rr1@outlook.com>
2026-02-19 20:25:12 -0600

    [Review 2.0] Code of Leetcode 76

--- src/LeetCode76/Review/ReviewV2.java
+++ src/LeetCode76/Review/ReviewV2.java
@@ -1,5 +1,8 @@
 package LeetCode76.Review;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReviewV2 {
     // s = "ADOBECODEBANC" t = "ABC"
     // targetMap: {A:1, B:1, C:1}, required: 3
@@ -19,6 +22,50 @@ public class ReviewV2 {
     //      4. L++
     //  R++
     // RETURN substring(ans)
+
     public String minWindow(String s, String t) {
+        if (s.isEmpty() || t.isEmpty() || s.length() < t.length()) return "";
+
+        Map<Character, Integer> hashMapT = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            hashMapT.put(t.charAt(i), hashMapT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        int required = hashMapT.size();
+        int l = 0, r = 0;
+        int create = 0;
+        int[] ans = {-1, 0, 0};
+        Map<Character, Integer> subStringHashMap = new HashMap<>();
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            int count = subStringHashMap.getOrDefault(c, 0);
+            subStringHashMap.put(c, count + 1);
+            if (hashMapT.containsKey(c) && subStringHashMap.get(c).intValue() == hashMapT.get(c).intValue()) {
+                create++;
+            }
+
+            while (l <= r && required == create) {
+                c = s.charAt(l);
+                if (ans[0] == -1 || ans[0] >= r - l + 1) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                subStringHashMap.put(c, subStringHashMap.get(c) - 1);
+                if (hashMapT.containsKey(c) && subStringHashMap.get(c).intValue() < hashMapT.get(c).intValue()) {
+                    create--;
+                }
+                l++;
+            }
+            r++;
+        }
+
+        if (ans[0] == -1) {
+            return "";
+        } else {
+            return s.substring(ans[1], ans[2] + 1);
+        }
     }
 }
