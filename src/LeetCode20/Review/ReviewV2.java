22713ce81fb35f75fa93eb603e8b61174e4c22d4
erad-dev-mx <erick_rr1@outlook.com>
2026-02-23 19:57:00 -0600

    [Review 2.0] Thought process of LeetCode 20

--- src/LeetCode20/Review/ReviewV2.java
+++ src/LeetCode20/Review/ReviewV2.java
@@ -1,6 +1,20 @@
 package LeetCode20.Review;
 
 public class ReviewV2 {
+    // '(',')','[',']','{','}'
+
+    // Stack -> LIFO
+    // HashMap = { '(',')','[',']','{','}' }
+
+    // s = "{[]}"
+    // char='{': Not in map -> Stack: ['{']
+    // char='[': Not in map -> Stack: ['{', '[']
+    // char=']': In map! Pair is '['. Stack pop is '['. Match! -> Stack: ['{']
+    // char='}': In map! Pair is '{'. Stack pop is '{'. Match! -> Stack: []
+    // Result: Stack is empty -> True.
+    //
+    // - "]" -> Stack empty on first closing char -> return False.
+    // - "([" -> Stack not empty at end -> return False.
     public boolean isValid(String s) {
 
     }
