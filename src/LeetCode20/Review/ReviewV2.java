bb83cf5f5dd854b3af1463b0e1680899e02c5fe7
erad-dev-mx <erick_rr1@outlook.com>
2026-02-23 19:57:30 -0600

    [Review 2.0] Code of LeetCode 20

--- src/LeetCode20/Review/ReviewV2.java
+++ src/LeetCode20/Review/ReviewV2.java
@@ -1,5 +1,8 @@
 package LeetCode20.Review;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class ReviewV2 {
     // '(',')','[',']','{','}'
 
@@ -16,6 +19,26 @@ public class ReviewV2 {
     // - "]" -> Stack empty on first closing char -> return False.
     // - "([" -> Stack not empty at end -> return False.
     public boolean isValid(String s) {
+        HashMap<Character, Character> brackets = new HashMap<>();
+        brackets.put(')', '(');
+        brackets.put(']', '[');
+        brackets.put('}', '{');
 
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!brackets.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char topElement = stack.pop();
+                if (topElement != brackets.get(c)) return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
