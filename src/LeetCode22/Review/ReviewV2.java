7ee14abb568561c8b547155f6e44995f22039f2c
erad-dev-mx <erick_rr1@outlook.com>
2026-03-02 20:33:43 -0600

    [Review 2.0] Though process of LeetCode 22

--- src/LeetCode22/Review/ReviewV2.java
+++ src/LeetCode22/Review/ReviewV2.java
@@ -3,6 +3,15 @@ package LeetCode22.Review;
 import java.util.List;
 
 public class ReviewV2 {
+    // 1. Placement: Add '(' if open < n
+    // 2. Validity:  Add ')' if close < open (prevents starting with ')' or closing too many)
+    // 3. Base Case: If length == 2 * n, we found a valid combination.
+
+    // (open, close) -> path
+    // (0,0) ""
+    //      (1,0) "("
+    //      (2,0) "((" -> (2,1) "(()" -> (2,2) "(())" [BASE CASE]
+    //      (1,1) "()" -> (2,1) "()(" -> (2,2) "()()" [BASE CASE]
     public List<String> generateParenthesis(int n) {
 
     }
