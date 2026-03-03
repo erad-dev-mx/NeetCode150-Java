e0f995e0b8436bdb5a982ba9306ce41d1f972602
erad-dev-mx <erick_rr1@outlook.com>
2026-03-02 20:34:10 -0600

    [Review 2.0] Code of LeetCode 22

--- src/LeetCode22/Review/ReviewV2.java
+++ src/LeetCode22/Review/ReviewV2.java
@@ -1,5 +1,6 @@
 package LeetCode22.Review;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewV2 {
@@ -13,6 +14,34 @@ public class ReviewV2 {
     //      (2,0) "((" -> (2,1) "(()" -> (2,2) "(())" [BASE CASE]
     //      (1,1) "()" -> (2,1) "()(" -> (2,2) "()()" [BASE CASE]
     public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
 
+        backtracking(ans, new StringBuilder(), 0, 0, n);
+
+        return ans;
+    }
+
+    private void backtracking(
+            List<String> ans,
+            StringBuilder current,
+            int open,
+            int close,
+            int max
+    ) {
+        if (current.length() == max * 2) {
+            ans.add(current.toString());
+            return;
+        }
+
+        if (open < max) {
+            current.append("(");
+            backtracking(ans, current, open + 1, close, max);
+            current.deleteCharAt(current.length() - 1); // BACKTRACK: Clean up for next branch
+        }
+        if (close < open) {
+            current.append(")");
+            backtracking(ans, current, open, close + 1, max);
+            current.deleteCharAt(current.length() - 1); // BACKTRACK: Clean up for next branch
+        }
     }
 }
