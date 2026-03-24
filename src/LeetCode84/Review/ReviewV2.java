b9427c75802f604fa7d811455d712437b71769a0
erad-dev-mx <erick_rr1@outlook.com>
2026-03-23 21:02:58 -0600

    [Review 2.0] Code of LeetCode 84

--- src/LeetCode84/Review/ReviewV2.java
+++ src/LeetCode84/Review/ReviewV2.java
@@ -6,22 +6,22 @@ public class ReviewV2 {
     // I use an increasing stack
     // When I see a smaller height, I calculate area for previous bars.
     // Each smaller height will be pop out from stack.
-    // heights = [2,1,5,6,2,3]
-    // i=0 → height=2 → push → [0]
-    // i=1 → height=1
-    // 1 < 2 → pop 2
-    // height=2, width=1 → area=2
-    // push 1 → [1]
-    //i=2 → height=5 → push → [1,2]
-    //i=3 → height=6 → push → [1,2,3]
-    //i=4 → height=2
-    // 2 < 6 → pop 6
-    // width = 1 → area = 6
-    // 2 < 5 → pop 5
-    // width = 2 → area = 10  ✅ (max so far)
-    // push 4 → [1,4]
-    // i=5 → height=3 → push → [1,4,5]
-    // i=6 → height=0 (fake end)
+    // heights = [2, 1, 5, 6, 2, 3]
+    // i = 0 -> height = 2 -> push -> [0]
+    // i = 1 → height = 1
+    // 1 < 2 -> pop 2
+    // height = 2, width = 1 -> area = 2
+    // push 1 -> [1]
+    // i = 2 -> height = 5 -> push -> [1,2]
+    // i = 3 -> height = 6 -> push -> [1,2,3]
+    // i = 4 -> height = 2
+    // 2 < 6 -> pop 6
+    // width = 1 -> area = 6
+    // 2 < 5 -> pop 5
+    // width = 2 -> area = 10 (max so far)
+    // push 4 -> [1,4]
+    // i = 5 -> height = 3 -> push -> [1, 4, 5]
+    // i = 6 -> height = 0 (fake end)
     // pop all remaining:
     // compute remaining areas
     public int largestRectangleArea(int[] heights) {
