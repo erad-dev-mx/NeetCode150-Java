60dcf33696bbe9a1179ce4bc6e712234d02b2e97
erad-dev-mx <erick_rr1@outlook.com>
2026-03-24 18:31:31 -0600

    [Review 2.0] Code of LeetCode 704

--- src/LeetCode704/Review/ReviewV2.java
+++ src/LeetCode704/Review/ReviewV2.java
@@ -10,6 +10,21 @@ public class ReviewV2 {
     // --- Update the value of left pointer
     // If we don't found an answer, then return -1
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
     }
 }
