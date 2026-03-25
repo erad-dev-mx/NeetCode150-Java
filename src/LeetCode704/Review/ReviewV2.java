5744a27f86251e072d850bedad96535d27f0b9fe
erad-dev-mx <erick_rr1@outlook.com>
2026-03-24 18:31:12 -0600

    [Review 2.0] Thought process of LeetCode 704

--- src/LeetCode704/Review/ReviewV2.java
+++ src/LeetCode704/Review/ReviewV2.java
@@ -1,6 +1,14 @@
 package LeetCode704.Review;
 
 public class ReviewV2 {
+    // O(log n)
+    // array of integers sorted in ascendig order
+    // Two variables: left, right
+    // Then, we get the middle value
+    // We will check if middle is greater than target
+    // --- Update the value of right pointer
+    // --- Update the value of left pointer
+    // If we don't found an answer, then return -1
     public int search(int[] nums, int target) {
 
     }
