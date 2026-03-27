efebfd25ebbe3e145573d98c253a0d3b8766dc38
erad-dev-mx <erick_rr1@outlook.com>
2026-03-26 19:46:26 -0600

    [Review 2.0] Thought process of LeetCode 74

--- src/LeetCode74/Review/ReviewV2.java
+++ src/LeetCode74/Review/ReviewV2.java
@@ -1,6 +1,18 @@
 package LeetCode74.Review;
 
 public class ReviewV2 {
+    // Brute force: using loops but since we need O(log(m *n)) this is not possible
+    // Binary Search
+    // matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    // middle = 12 / 2 = 6; val at index 6 = 11
+    // middle < target = 6 < 3 ? no : right = middle - 1
+    // matrix = [[1,3,5,7],[10,11]], target = 3
+    // middle = 6 / 2 = 3
+    // middle < target = 5 < 3 ? no : right = middle - 1
+    // matrix = [[1,3]
+    // middle = 2 / 2 = 1
+    // middle < target = 1 < 3 : left = middle + 1
+    // middle == target
     public boolean searchMatrix(int[][] matrix, int target) {
 
     }
