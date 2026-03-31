a5403183e66bc1b073ba94b3afc08ba2e39b6f89
erad-dev-mx <erick_rr1@outlook.com>
2026-03-30 20:34:31 -0600

    [Review 2.0] Code of LeetCode 153

--- src/LeetCode153/Review/ReviewV2.java
+++ src/LeetCode153/Review/ReviewV2.java
@@ -15,6 +15,24 @@ public class ReviewV2 {
     // nums = [0, 1]
     // l = 0, r = 1, m = 0 => 0 < 1 (range is sorted), ans = min(2, 0) = 0
     public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int answer = nums[0];
 
+        if (nums.length == 1) return nums[0];
+
+        while (left <= right) {
+            if (nums[left] < nums[right]) answer = Math.min(answer, nums[left]);
+
+            int mid = left + (right - left) / 2;
+            answer = Math.min(answer, nums[mid]);
+            if (nums[left] <= nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
     }
 }
