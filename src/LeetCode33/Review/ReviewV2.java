a1bd636b2b513e5f3e4e8549e12a5c29057afc07
erad-dev-mx <erick_rr1@outlook.com>
2026-04-01 19:54:40 -0600

    [Review 2.0] Code of LeetCode 33

--- src/LeetCode33/Review/ReviewV2.java
+++ src/LeetCode33/Review/ReviewV2.java
@@ -8,6 +8,29 @@ public class ReviewV2 {
     // L = 0, R = 2, M = 1
     // L == Target then we return answer
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (target < nums[left] || target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            } else {
+                if (target > nums[right] || target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
