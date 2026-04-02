55c4c0f72648c54562a435439fd98b2e0032c802
erad-dev-mx <erick_rr1@outlook.com>
2026-04-01 19:48:08 -0600

    [Review 2.0] Thought process of LeetCode 33

--- src/LeetCode33/Review/ReviewV2.java
+++ src/LeetCode33/Review/ReviewV2.java
@@ -1,6 +1,12 @@
 package LeetCode33.Review;
 
 public class ReviewV2 {
+    // [3, 4, 5, 6, 7, 0, 1, 2]
+    // Target = 0
+    // L = 3, R = 2, M = 7 => L < M (First portion is sorted)
+    // Target is not in first portion
+    // L = 0, R = 2, M = 1
+    // L == Target then we return answer
     public int search(int[] nums, int target) {
 
     }
