f140d876c815424a405f872a180a5dd8b7b98aa5
erad-dev-mx <erick_rr1@outlook.com>
2026-04-07 19:44:46 -0600

    [Review 2.0] Init of LeetCode 981

---
+++ src/LeetCode981/Review/ReviewV2.java
@@ -0,0 +1,16 @@
+package LeetCode981.Review;
+
+class TimeMapReviewV2 {
+
+    public TimeMapReviewV2() {
+
+    }
+
+    public void set(String key, String value, int timestamp) {
+
+    }
+
+    public String get(String key, int timestamp) {
+
+    }
+}
