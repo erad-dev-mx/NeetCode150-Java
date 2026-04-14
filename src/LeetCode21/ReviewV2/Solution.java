d0ab02b9d3adb7a9cd7fd100f5a2ccff499dedd1
erad-dev-mx <erick_rr1@outlook.com>
2026-04-13 20:56:54 -0600

    [Review 2.0] Thought process of LeetCode 21

--- src/LeetCode21/ReviewV2/Solution.java
+++ src/LeetCode21/ReviewV2/Solution.java
@@ -11,6 +11,13 @@ package LeetCode21.ReviewV2;
  * }
  */
 class Solution {
+    // l1 = (1), (2), (4)
+    // l2 = (1), (3), (4)
+    // Pointers[1,1] => m = (1) - (1)
+    // Pointers[2,3] => m = (1) - (1) - (2)
+    // Pointers[4,3] => m = (1) - (1) - (2) - (3)
+    // Pointers[4,4] => m = (1) - (1) - (2) - (3) - (4) - (4)
+    // Dummy will point to m head
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
     }
