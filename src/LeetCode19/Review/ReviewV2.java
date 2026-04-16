706fda54e84e8b2165f710aec4a9abadf8441cee
erad-dev-mx <erick_rr1@outlook.com>
2026-04-15 20:31:09 -0600

    [Review 2.0] Code of LeetCode 19

--- src/LeetCode19/Review/ReviewV2.java
+++ src/LeetCode19/Review/ReviewV2.java
@@ -20,6 +20,23 @@ public class ReviewV2 {
     // FP is equal to null, then BP.next = BP.next.next
     // 5th Iteration: (1)->(2)->(3)->(4)->(6)->null[FP]
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(1);
+        dummy.next = head;
 
+        ListNode front = dummy;
+        ListNode back = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            front = front.next;
+        }
+
+        while (front != null) {
+            front = front.next;
+            back = back.next;
+        }
+
+        back.next = back.next.next;
+
+        return dummy.next;
     }
 }
