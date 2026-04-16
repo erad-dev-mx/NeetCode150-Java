ec5426023ca50a42825bea230625d1c287be9ae8
erad-dev-mx <erick_rr1@outlook.com>
2026-04-15 20:30:47 -0600

    [Review 2.0] Thought process of LeetCode 19

--- src/LeetCode19/Review/ReviewV2.java
+++ src/LeetCode19/Review/ReviewV2.java
@@ -11,6 +11,14 @@ package LeetCode19.Review;
  * }
  */
 public class ReviewV2 {
+    // Example: (1)->(2)->(3)->(4)->(5)->(6)
+    // n = 2
+    // 1st Iteration: (1[BP])->(2)->(3)->(4[FP])->(5)->(6)->null
+    // 2nd Iteration: (1)->(2[BP])->(3)->(4)->(5[FP])->(6)->null
+    // 3rd Iteration: (1)->(2)->(3[BP])->(4)->(5)->(6[FP])->null
+    // 4th Iteration: (1)->(2)->(3)->(4[BP])->(5)->(6[FP])->null[FP]
+    // FP is equal to null, then BP.next = BP.next.next
+    // 5th Iteration: (1)->(2)->(3)->(4)->(6)->null[FP]
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
     }
