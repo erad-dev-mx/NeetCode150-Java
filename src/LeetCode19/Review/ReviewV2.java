7e137451c385b9970644d11d7cf0fec0e5b56d8f
erad-dev-mx <erick_rr1@outlook.com>
2026-04-15 20:31:39 -0600

    [Review 2.0] Code refactor of LeetCode 19

--- src/LeetCode19/Review/ReviewV2.java
+++ src/LeetCode19/Review/ReviewV2.java
@@ -3,11 +3,11 @@ package LeetCode19.Review;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 public class ReviewV2 {
