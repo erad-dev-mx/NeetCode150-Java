1968e921c2fd4b4535b4487aa37aa418f849e33d
erad-dev-mx <erick_rr1@outlook.com>
2026-04-15 20:12:07 -0600

    [Review 2.0] Init of LeetCode 19

---
+++ src/LeetCode19/Review/ReviewV2.java
@@ -0,0 +1,17 @@
+package LeetCode19.Review;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ReviewV2 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+}
