6fd412c9baa2aa06a0b93f22b68c78e2abf20336
erad-dev-mx <erick_rr1@outlook.com>
2026-04-20 20:21:34 -0600

    [Review 2.0] Init of LeetCode 138

---
+++ src/LeetCode138/Review/ReviewV2.java
@@ -0,0 +1,21 @@
+package LeetCode138.Review;
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class ReviewV2 {
+    public Node copyRandomList(Node head) {
+    }
+}
