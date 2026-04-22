6ee44a426c71f883597b094b958a46f630644794
erad-dev-mx <erick_rr1@outlook.com>
2026-04-21 20:18:15 -0600

    [Review 2.0] Thought process of LeetCode 2

--- src/LeetCode2/Review/ReviewV2.java
+++ src/LeetCode2/Review/ReviewV2.java
@@ -1,6 +1,14 @@
 package LeetCode2.Review;
 
 public class ReviewV2 {
+    // l1 = (8) -> (0) -> (7)
+    // l2 = (3) -> (0) -> (5)
+    // var carry = 0
+    // dummy = (1) -> () -> () --- carry = 1
+    // dummy = (1) -> (1) -> () --- carry = 0
+    // dummy = (1) -> (1) -> (2) --- carry = 1
+    // dummy = (1) -> (1) -> (2) -> (1) --- carry = 0
+    // Ans = 1211
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
     }
