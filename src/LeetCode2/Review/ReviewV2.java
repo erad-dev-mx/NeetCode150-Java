815e42c9fad1d4897f40f1b06b4644668c76148e
erad-dev-mx <erick_rr1@outlook.com>
2026-04-21 20:23:19 -0600

    [Review 2.0] Code of LeetCode 2

--- src/LeetCode2/Review/ReviewV2.java
+++ src/LeetCode2/Review/ReviewV2.java
@@ -10,6 +10,24 @@ public class ReviewV2 {
     // dummy = (1) -> (1) -> (2) -> (1) --- carry = 0
     // Ans = 1211
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode ans = dummy;
+        int carry = 0;
 
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+
+            ans.next = new ListNode(sum % 10);
+            ans = ans.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
     }
 }
