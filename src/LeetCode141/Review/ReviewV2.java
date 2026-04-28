0e5dda12367f518e96d52b2c9bad96e5d80992fb
erad-dev-mx <erick_rr1@outlook.com>
2026-04-27 20:06:18 -0600

    [Review 2.0] Thought process of LeetCode 141

--- src/LeetCode141/Review/ReviewV2.java
+++ src/LeetCode141/Review/ReviewV2.java
@@ -12,6 +12,9 @@ package LeetCode141.Review;
  * }
  */
 public class ReviewV2 {
+    // We can use two pointers: slow and fast
+    // If there is a cycle in some point they will be at the same position
+    // If there is not a cycle the faster will be at null
     public boolean hasCycle(ListNode head) {
 
     }
