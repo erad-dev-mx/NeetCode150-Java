46d6ca3ed8146a0413273add72d94e5521b590d8
erad-dev-mx <erick_rr1@outlook.com>
2026-04-27 20:06:41 -0600

    [Review 2.0] Code of LeetCode 141

--- src/LeetCode141/Review/ReviewV2.java
+++ src/LeetCode141/Review/ReviewV2.java
@@ -16,6 +16,20 @@ public class ReviewV2 {
     // If there is a cycle in some point they will be at the same position
     // If there is not a cycle the faster will be at null
     public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
 
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null || fast.next != null) {
+            if (fast.next == null) return false;
+
+            if (fast == slow) return true;
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
     }
 }
