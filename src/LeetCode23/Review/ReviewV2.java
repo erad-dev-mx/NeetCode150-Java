71db96eef172aed912b488daeacd44f2099ae6f2
erad-dev-mx <erick_rr1@outlook.com>
2026-05-12 19:19:50 -0600

    [Review 2.0] Thought process of LeetCode 23

--- src/LeetCode23/Review/ReviewV2.java
+++ src/LeetCode23/Review/ReviewV2.java
@@ -11,6 +11,11 @@ package LeetCode23.Review;
  * }
  */
 public class ReviewV2 {
+    // We can try using a PriorityQueue representing the MinHeap
+    // l1 = 4, 7; l2 = 3, 8; l3 1, 18
+    // minHeap = [4,7] => minHeap = [3,4,7,8] => minHeap = [1,3,4,7,8,18]
+    // mergeList = 1,3,4,...
+    // Time Complexity = O (n log n), Space Complexity = O (n)
     public ListNode mergeKLists(ListNode[] lists) {
 
     }
