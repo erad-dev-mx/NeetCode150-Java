a8dfd629bd7eaf20768ca36ecc84366093971325
erad-dev-mx <erick_rr1@outlook.com>
2026-05-14 20:09:30 -0600

    [Review 2.0] Code of LeetCode 25

--- src/LeetCode25/Review/ReviewV2.java
+++ src/LeetCode25/Review/ReviewV2.java
@@ -31,6 +31,51 @@ public class ReviewV2 {
     // To do an optimal solution we could use recursion - Time: O(n), Space: O(n)
     // If we want something better we could use iterative approach - Time (n), Space O(1)
     public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode ptr = head;
+        ListNode kTail = null;
+        ListNode newHead = null;
 
+        while (ptr != null) {
+            int count = 0;
+            ptr = head;
+
+            while (count < k && ptr != null) {
+                ptr = ptr.next;
+                count++;
+            }
+
+            if (count == k) {
+                ListNode revHead = reverseLinkedList(head, k);
+
+                if (newHead == null) newHead = revHead;
+
+                if (kTail != null) kTail.next = revHead;
+
+                kTail = head;
+                head = ptr;
+            }
+        }
+
+        if (kTail != null) {
+            kTail.next = head;
+        }
+
+        return newHead == null ? head : newHead;
+    }
+
+    private ListNode reverseLinkedList(ListNode head, int k) {
+        ListNode newHead = null;
+        ListNode ptr = head;
+
+        while (k > 0) {
+            ListNode nextNode = ptr.next;
+            ptr.next = newHead;
+            newHead = ptr;
+            ptr = nextNode;
+
+            k--;
+        }
+
+        return newHead;
     }
 }
