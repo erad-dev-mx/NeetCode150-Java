5be1763309ce80b427276dcf6b0d0d808c1e41ca
erad-dev-mx <erick_rr1@outlook.com>
2026-05-18 20:19:47 -0600

    [Review 2.0] Thought process of LeetCode 226

--- src/LeetCode226/Review/ReviewV2.java
+++ src/LeetCode226/Review/ReviewV2.java
@@ -16,6 +16,12 @@ package LeetCode226.Review;
  * }
  */
 public class ReviewV2 {
+    // In order to dolve this problem
+    // We should swap right child with left child
+    // [1,2,3,4,5,6,7]
+    // [1,3,2,4,5,6,7]
+    // [1,3,2,7,6,4,5]
+    // [1,3,2,7,6,5,4] => This is the answer
     public TreeNode invertTree(TreeNode root) {
 
     }
