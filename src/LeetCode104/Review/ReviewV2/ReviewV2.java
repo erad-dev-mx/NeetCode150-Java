317fc46c790ffe8de0f611ceeb4dd4d122490626
erad-dev-mx <erick_rr1@outlook.com>
2026-05-19 20:10:47 -0600

    [Review 2.0] Init of LeetCode 104

---
+++ src/LeetCode104/Review/ReviewV2/ReviewV2.java
@@ -0,0 +1,24 @@
+package LeetCode104.Review.ReviewV2;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ReviewV2 {
+    public int maxDepth(TreeNode root) {
+        
+    }
+}
