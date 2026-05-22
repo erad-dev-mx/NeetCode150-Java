e6535c99f8c27b696d43fc3758bc2c4222491e8e
erad-dev-mx <erick_rr1@outlook.com>
2026-05-21 20:07:32 -0600

    [Review 2.0] Code of LeetCode 543

--- src/LeetCode543/Review/ReviewV2.java
+++ src/LeetCode543/Review/ReviewV2.java
@@ -15,11 +15,26 @@ package LeetCode543.Review;
  *     }
  * }
  */
-public class ReviewV2 {
-    // Diameter = left_height + right_height
-    // Returns Height = Max(left, right) + 1
-    // Return the height, but update the global diameter in the process.
-    public int diameterOfBinaryTree(TreeNode root) {
 
+// Diameter = left_height + right_height
+// Returns Height = Max(left, right) + 1
+// Return the height, but update the global diameter in the process.
+public class ReviewV2 {
+    int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        getHeight(root);
+        return maxDiameter;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
