0fa06f518848d61d431fa55a4d1f99dabf28310d
erad-dev-mx <erick_rr1@outlook.com>
2026-05-23 01:02:36 -0600

    [Review 2.0] Code of LeetCode 110

--- src/LeetCode110/Review/ReviewV2.java
+++ src/LeetCode110/Review/ReviewV2.java
@@ -25,5 +25,22 @@ public class ReviewV2 {
     // Absolute Difference (leftHeigh - rightHeight) > 1 is INVALID
     // The Height Balance Tree
     public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
