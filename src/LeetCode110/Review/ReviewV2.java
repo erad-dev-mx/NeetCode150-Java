80a616c6700f3a77778aebe6ff881ee22c99cf1e
erad-dev-mx <erick_rr1@outlook.com>
2026-05-23 00:52:03 -0600

    [Review 2.0] Setting up of LeetCode 110

--- src/LeetCode110/Review/ReviewV2.java
+++ src/LeetCode110/Review/ReviewV2.java
@@ -1,5 +1,23 @@
 package LeetCode110.Review;
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 public class ReviewV2 {
-    
+    public boolean isBalanced(TreeNode root) {
+
+    }
 }
