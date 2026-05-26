284eeba332e40cc3e0730d1a7f28fe5c29742cb4
erad-dev-mx <erick_rr1@outlook.com>
2026-05-25 19:49:20 -0600

    [Review 2.0] Code of LeetCode 100

--- src/LeetCode100/Review/ReviewV2.java
+++ src/LeetCode100/Review/ReviewV2.java
@@ -26,6 +26,10 @@ public class ReviewV2 {
     // First, checking all left subtrees and then, the right subtree
     // Time = O(N), Space = O (long n)
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
