6bcfeb3781155c2a2ff3c1a96433173cbd832623
erad-dev-mx <erick_rr1@outlook.com>
2026-05-25 19:39:27 -0600

    [Review 2.0] Thought process of LeetCode 100

--- src/LeetCode100/Review/ReviewV2.java
+++ src/LeetCode100/Review/ReviewV2.java
@@ -16,6 +16,15 @@ package LeetCode100.Review;
  * }
  */
 public class ReviewV2 {
+    // Approach 1
+    // Input: p = [1,2,3], q = [1,2,3]; Output = true
+    // We traverse both arrays comparing two elements
+    // If we found differences return false
+    // If not, return true
+    // Input: p = [1,2], q = [1,null,2]; Output = false
+    // A very good way to solve is using recursion
+    // First, checking all left subtrees and then, the right subtree
+    // Time = O(N), Space = O (long n)
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
     }
