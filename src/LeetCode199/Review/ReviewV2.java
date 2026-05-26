c01b5cb900ee5f9ad57e2a84bf5669b5b7734dfe
erad-dev-mx <erick_rr1@outlook.com>
2026-05-25 19:51:32 -0600

    [Review 2.0] Setting up of LeetCode 199

--- src/LeetCode199/Review/ReviewV2.java
+++ src/LeetCode199/Review/ReviewV2.java
@@ -1,4 +1,24 @@
 package LeetCode199.Review;
 
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class ReviewV2 {
+    public List<Integer> rightSideView(TreeNode root) {
+
+    }
 }
