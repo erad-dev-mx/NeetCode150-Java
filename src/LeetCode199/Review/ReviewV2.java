9532549649dc1c176194232c647b41bb396f5701
erad-dev-mx <erick_rr1@outlook.com>
2026-05-25 20:09:44 -0600

    [Review 2.0] Thought process of LeetCode 199

--- src/LeetCode199/Review/ReviewV2.java
+++ src/LeetCode199/Review/ReviewV2.java
@@ -18,6 +18,12 @@ import java.util.List;
  * }
  */
 public class ReviewV2 {
+    // To solve this, we could use BFS
+    // Starting in root, then put it in a queue
+    // Go down to all the children, and again we put the values in the queue
+    // Example: [1, 2, 3, 4, 5, null, 6, null, null, 7, null]
+    // Q = [1] => [3, 2, 1] => [5, 4, 3, 2, 1] => [6, 5, 4, 3, 2, 1] = [7, 6, 5, 4, 3, 2, 1]
+    // Ans = [1, 3, 6, 7]
     public List<Integer> rightSideView(TreeNode root) {
 
     }
