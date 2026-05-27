d21031b8a96d98aa55a5b1ec492b1c2ef8956bc2
erad-dev-mx <erick_rr1@outlook.com>
2026-05-26 20:18:19 -0600

    [Review] Thought process of LeetCode 1448

--- src/LeetCode1448/Review/Solution.java
+++ src/LeetCode1448/Review/Solution.java
@@ -16,6 +16,18 @@ package LeetCode1448.Review;
  * }
  */
 class Solution {
+    // We will use DFS
+    // First, we will check root at any given position
+    // Then, left entire subtree and right entire subtree
+    // So we use Pre Order Traversal (Root, Left, Right)
+    // We keep track of Max Value. In each Current Node we will evaluate if it is greater.
+    // (3)
+    // (1) (4)
+    // (3) (5) null (3)
+    // MV = 3, 1 is not a good node, 3 is a good node; count = 2, 5 is a good node; count = 3; there is no child so we
+    // avoid updating MV.
+    // MV = 3, 4 is a good node; MV = 4; count = 4, 2 is not a good node
+    // We return 4
     public int goodNodes(TreeNode root) {
         
     }
