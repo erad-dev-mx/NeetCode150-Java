33c003b325464c37c31d406b6bae8b3026de1c6a
erad-dev-mx <erick_rr1@outlook.com>
2026-05-28 20:21:19 -0600

    [Init] Code of LeetCode 235

---
+++ src/LeetCode235/Review/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode235.Review;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+}
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
