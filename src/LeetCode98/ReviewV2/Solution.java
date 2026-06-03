0704a06bcaebda2a2205766303bde9f7bd0b0bc3
erad-dev-mx <erick_rr1@outlook.com>
2026-06-02 20:16:23 -0600

    [Review] Thought process of LeetCode 98

--- src/LeetCode98/ReviewV2/Solution.java
+++ src/LeetCode98/ReviewV2/Solution.java
@@ -16,6 +16,20 @@ package LeetCode98.ReviewV2;
  * }
  */
 class Solution {
+    // (5)
+    // (3)      (8)
+    // (1) (4)  (2) (9)
+    // We start with our root validating if values are lower and higher.
+    // Until the end, but there is a problem. We will have a false positive in 8.
+    // Why? 2 is less than 8 and 9 is greater than 8 but by definition everything on the right side should be
+    // greater than 5 but in this case 2.
+    // In Order Traversal: Left -> Node -> Right
+    // (5)
+    // (3)      (8)
+    // (1) (4)  (6) (9)
+    // Visiting the prev tree: 1, 3, 4, 5, 6, 8, 9
+    // We will just check if the val + 1 is greater than val.
+    // Traversal in first tree: 1, 3, 4, 5, 2 -> invalid, so we return false
     public boolean isValidBST(TreeNode root) {
         
     }
