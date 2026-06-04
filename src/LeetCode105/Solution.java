95b07fcec602a0cf1426f8c350153d9a3623a23f
erad-dev-mx <erick_rr1@outlook.com>
2026-06-03 19:49:47 -0600

    [First try] Init of LeetCode 105

---
+++ src/LeetCode105/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode105;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
