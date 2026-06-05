a485a0193b445191a61567d86bb9e4c35076e0e9
erad-dev-mx <erick_rr1@outlook.com>
2026-06-04 20:14:04 -0600

    [First try] Thought process of LeetCode 124

---
+++ src/LeetCode124/Solution.java
@@ -0,0 +1,37 @@
+package LeetCode124;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // A valid path is basically when a path doesn't repaet nodes
+    // For one root we will calculte left and right subtree to divide the problem
+    public int maxPathSum(TreeNode root) {
+        
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
