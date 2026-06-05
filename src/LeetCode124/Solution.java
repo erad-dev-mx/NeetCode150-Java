9da9ae380f521e9d4980c02049ffe965109fae56
erad-dev-mx <erick_rr1@outlook.com>
2026-06-04 20:19:09 -0600

    [First try] Code of LeetCode 124

--- src/LeetCode124/Solution.java
+++ src/LeetCode124/Solution.java
@@ -18,8 +18,24 @@ package LeetCode124;
 class Solution {
     // A valid path is basically when a path doesn't repaet nodes
     // For one root we will calculte left and right subtree to divide the problem
+
+    int maxSum = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        
+        maxGain(root);
+
+        return maxSum;
+    }
+
+    private int maxGain(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+        maxSum = Math.max(maxSum, priceNewPath);
+
+        return node.val + Math.max(leftGain, rightGain);
     }
 }
 
