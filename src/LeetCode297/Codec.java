699123b8100727601fc6ff287801c4ba57a79783
erad-dev-mx <erick_rr1@outlook.com>
2026-06-09 20:12:53 -0600

    [First try] Thought process of LeetCode 297

--- src/LeetCode297/Codec.java
+++ src/LeetCode297/Codec.java
@@ -9,6 +9,19 @@ package LeetCode297;
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// We need to traverse the ree, searialize into a string and then, back to tree
+// To searialize tree, we use preOrder traversal with DFS: Root -> Left -> Right
+//      (1)
+//   (2)      (3)
+// (4) (5)  null null
+// s = 1, 2, 4, null, null, 5, null, null, 3, null, null
+// To deserialize string
+// s = 1, 2, 4, null, null, 5, null, null, 3, null, null
+// Again using preOrder Traversal
+//      (1)
+//  (2)     (3)
+// (4) (5)
 public class Codec {
 
     // Encodes a tree to a single string.
