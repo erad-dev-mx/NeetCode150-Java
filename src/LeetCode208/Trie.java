196f4a07b0c45049d8bd1d2e84a460824f4bb079
erad-dev-mx <erick_rr1@outlook.com>
2026-06-10 20:39:09 -0600

    [First try] Thought process II of LeetCode 297

--- src/LeetCode208/Trie.java
+++ src/LeetCode208/Trie.java
@@ -15,6 +15,16 @@ package LeetCode208;
 // For google we create a new branch with g, o, o, g, l, e, end
 // For amazon we can use a from apple and then create a new branch with m, a, z, o, n, end
 // For amazing a, m, a, z are already present so we create a new branch in z with i, n, g, end
+// SEARCH METHOD: amazing, good, googler, app
+// amazing, we check if on of our children starts with a and then we try to complete all the word (TRUE)
+// good, g is present, then two o's are present but we don't have a branch with d (FALSE)
+// google, until e everhing exists but there is no r (FALSE)
+// app, three words exist but there is no end (FALSE)
+// STARTS WITH METHOD: ama, apk, good, apple
+// ama, there is present in amazon/amazing path (TRUE)
+// apk, in the p we don't have a k children (FALSE)
+// good, the last o don't have a d children (FALSE)
+// apple, we have the word (TRUE)
 class Trie {
 
     public Trie() {
