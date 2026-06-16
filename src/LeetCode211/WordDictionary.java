0fdc7d8a183d90f40b6b145ab47293ee2853d189
erad-dev-mx <erick_rr1@outlook.com>
2026-06-15 20:23:58 -0600

    [First try] Thought process of LeetCode 211

--- src/LeetCode211/WordDictionary.java
+++ src/LeetCode211/WordDictionary.java
@@ -1,5 +1,16 @@
 package LeetCode211;
 
+// We will use TRIE
+// Added words:
+// pen, pot, cat, cars, bed
+// Words to be search:
+// pot, pts, .ar, ..t, ..ey
+// - pot: TRUE
+// - pots: We could find the last s so FALSE
+// - .ar: when we see the dot we will search in each node
+// until cars path we will find a match for this word. The answer is TRUE
+// - ..t: TRUE
+// .ey: there is no present in our true. We return FALSE
 class WordDictionary {
 
     public WordDictionary() {
