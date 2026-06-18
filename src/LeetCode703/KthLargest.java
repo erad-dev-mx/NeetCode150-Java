34e5189a620cca87851341a0e191558be41d6192
erad-dev-mx <erick_rr1@outlook.com>
2026-06-17 20:54:14 -0600

    [First try] Thinking process of LeetCode 703

--- src/LeetCode703/KthLargest.java
+++ src/LeetCode703/KthLargest.java
@@ -1,5 +1,21 @@
 package LeetCode703;
 
+// k = 3, nums = [4, 5, 8, 2]
+// We will use Min-Heap (PriorityQueue) with k length
+// The root (peek) always contains kth largest element
+// kthLargest(int k, int[] nums)
+// num = 4 -> heap: [4] (length < 3)
+// num = 5 -> heap: [4, 5] (length < 3)
+// num = 8 -> heap: [4, 5, 8] (length == 3)
+// num = 2 -> 2 <= heap.peek(4) -> ignore
+// Final initial heap [4, 5, 8], return null
+// add(int val)
+// add(3) -> 3 <= 4 (peek) -> ignore. Heap: [4, 5, 8] -> return 4
+// add(5) -> 5 > 4 (peek)  -> poll(4), push(5). Heap: [5, 5, 8] -> return 5
+// add(10)-> 10 > 5 (peek) -> poll(5), push(10). Heap: [5, 8, 10]-> return 5
+// add(9) -> 9 > 5 (peek)  -> poll(5), push(9). Heap: [8, 9, 10] -> return 8
+// add(4) -> 4 <= 8 (peek) -> ignore. Heap: [8, 9, 10] -> return 8
+// ans = [null, 4, 5, 5, 8, 8]
 class KthLargest {
 
     public KthLargest(int k, int[] nums) {
