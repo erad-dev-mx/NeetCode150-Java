0389f0b9dd647448f9f56926ed8a6524430cfbaa
erad-dev-mx <erick_rr1@outlook.com>
2026-07-27 19:36:47 -0600

    [First try] Thinking process of LeetCode 973

--- src/LeetCode973/Solution.java
+++ src/LeetCode973/Solution.java
@@ -1,6 +1,13 @@
 package LeetCode973;
 
 class Solution {
+    // Brute force
+    // We can simply start by calculating and then we will sort an find what is the K closest value
+    // Optimal
+    // We are going to use a MaxHeap with K length
+    // We will keep tracking the K closests value
+    // Time = O (n * log n)
+    // Space = O (k)
     public int[][] kClosest(int[][] points, int k) {
         
     }
