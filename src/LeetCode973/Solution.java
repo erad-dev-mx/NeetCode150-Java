ca4b6b48dd7e9fa147cdee90934526c623b87aca
erad-dev-mx <erick_rr1@outlook.com>
2026-07-27 19:37:57 -0600

    [First try] Code of LeetCode 973

--- src/LeetCode973/Solution.java
+++ src/LeetCode973/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode973;
 
+import java.util.PriorityQueue;
+
 class Solution {
     // Brute force
     // We can simply start by calculating and then we will sort an find what is the K closest value
@@ -9,6 +11,21 @@ class Solution {
     // Time = O (n * log n)
     // Space = O (k)
     public int[][] kClosest(int[][] points, int k) {
-        
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1]));
+
+        for (int[] point : points) {
+            maxHeap.add(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+
+        return result;
     }
 }
