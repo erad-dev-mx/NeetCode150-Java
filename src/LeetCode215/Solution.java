afd9adf0735dfd8a2a68c765cbf845af810a2b61
erad-dev-mx <erick_rr1@outlook.com>
2026-07-28 20:19:58 -0600

    [First try] Thinking process of LeetCode 215

--- src/LeetCode215/Solution.java
+++ src/LeetCode215/Solution.java
@@ -1,6 +1,9 @@
 package LeetCode215;
 
 class Solution {
+    // Brute Force
+    // We iterate through the entire array in order to find the max one
+    // Then, we find 2 max and so on until K max value
     public int findKthLargest(int[] nums, int k) {
         
     }
