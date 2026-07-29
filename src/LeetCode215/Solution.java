f7cd6d031be1a4cd06f4610678d3f5dcfca07241
erad-dev-mx <erick_rr1@outlook.com>
2026-07-28 20:28:55 -0600

    [First try] Code of LeetCode 215

--- src/LeetCode215/Solution.java
+++ src/LeetCode215/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode215;
 
+import java.util.PriorityQueue;
+
 class Solution {
     // Brute Force
     // We iterate through the entire array in order to find the max one
@@ -10,6 +12,13 @@ class Solution {
     // PriorityQueue is going to be a better approach
     // We will use a MinHeap (ascending order) with K size length
     public int findKthLargest(int[] nums, int k) {
-        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+
+        return minHeap.peek();
     }
 }
