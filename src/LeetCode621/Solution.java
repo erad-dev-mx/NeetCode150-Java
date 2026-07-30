a825cef67556457feaf4eb12f4c03955e41c39de
erad-dev-mx <erick_rr1@outlook.com>
2026-07-29 19:38:14 -0600

    [First try] Code of LeetCode 621

--- src/LeetCode621/Solution.java
+++ src/LeetCode621/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode621;
 
+import java.util.*;
+
 class Solution {
     // task = [A, B, A], n = 2
     // A -> B -> idle -> A ... return 4
@@ -26,6 +28,28 @@ class Solution {
     // Heap becomes empty
     // intervals = 7 ... return 7
     public int leastInterval(char[] tasks, int n) {
-        
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char t : tasks) {
+            freqMap.put(t, freqMap.getOrDefault(t, 0) + 1);
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        maxHeap.addAll(freqMap.values());
+
+        int time = 0;
+        while (!maxHeap.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < n + 1; i++) {
+                if (!maxHeap.isEmpty()) temp.add(maxHeap.poll());
+            }
+
+            for (int freq : temp) {
+                if (--freq > 0) maxHeap.add(freq);
+            }
+
+            time += maxHeap.isEmpty() ? temp.size() : n + 1;
+        }
+
+        return time;
     }
 }
