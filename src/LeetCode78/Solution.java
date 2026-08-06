677bdfaa5fd1a2b62bc0b56ca4a826500c7d7e51
erad-dev-mx <erick_rr1@outlook.com>
2026-08-05 20:10:57 -0600

    [First try] Code of LeetCode 78

--- src/LeetCode78/Solution.java
+++ src/LeetCode78/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode78;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
@@ -10,6 +11,18 @@ class Solution {
     //              [1,2,3]
     // We do this recursively + backtracking
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        generateSubsets(0, nums, new ArrayList<>(), result);
 
+        return result;
+    }
+
+    private void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            generateSubsets(i + 1, nums, current, result);
+            current.remove(current.size() - 1);
+        }
     }
 }
