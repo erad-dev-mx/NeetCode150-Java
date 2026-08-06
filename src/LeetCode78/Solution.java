775cc3d6ddfe3ca2dc374420b4d565d6befd567f
erad-dev-mx <erick_rr1@outlook.com>
2026-08-05 20:10:32 -0600

    [First try] Thinking process of LeetCode 78

--- src/LeetCode78/Solution.java
+++ src/LeetCode78/Solution.java
@@ -3,7 +3,13 @@ package LeetCode78;
 import java.util.List;
 
 class Solution {
+    // [1,2,3]
+    // At every single step, we are going to make a decision whether we include that element or not
+    // []           [1]             [2]         [3]
+    //              [1,2] [1,3]     [2,3]
+    //              [1,2,3]
+    // We do this recursively + backtracking
     public List<List<Integer>> subsets(int[] nums) {
-        
+
     }
 }
