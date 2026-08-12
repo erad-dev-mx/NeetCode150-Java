aab208ce8fa8d05e80090962f45b7f021ac714cd
erad-dev-mx <erick_rr1@outlook.com>
2026-08-11 19:23:44 -0600

    [First try] Code of LeetCode 39

--- src/LeetCode39/Solution.java
+++ src/LeetCode39/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode39;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
@@ -18,6 +19,21 @@ class Solution {
     // In last exaple when we are in 6, the only posible combination is (6,6)
     // Basically, we will use backtracking
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> combination = new ArrayList<>();
+
+        backtrack(target, res, combination, 0, candidates);
+        return res;
+    }
+
+    public void backtrack(int target, List<List<Integer>> res, List<Integer> combination, int start, int[] candidates) {
+        if (target == 0) res.add(new ArrayList<Integer>(combination));
+        if (target < 0) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            combination.add(candidates[i]);
+            backtrack(target - candidates[i], res, combination, i, candidates);
+            combination.remove(combination.size() - 1);
+        }
     }
 }
