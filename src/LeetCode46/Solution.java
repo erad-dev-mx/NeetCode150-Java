e80e288201479ed538642f1e442c0a9c8313cead
erad-dev-mx <erick_rr1@outlook.com>
2026-08-17 20:11:35 -0600

    [Init] Code of LeetCode 46

--- src/LeetCode46/Solution.java
+++ src/LeetCode46/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode46;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
@@ -14,6 +15,25 @@ class Solution {
     // currentVal: 1,3,2, used: true, true, true, ans = [1,3,2]
     // Then we go with to 2, until we reach 3
     public List<List<Integer>> permute(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(result, new ArrayList<>(), nums, used);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                current.add(nums[i]);
+                used[i] = true;
+                backtrack(result, current, nums, used);
+                used[i] = false;
+                current.remove(current.size() - 1);
+            }
+        }
     }
 }
