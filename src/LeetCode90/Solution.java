9a83c1887683ce07b852cb4534c25a979c687ce8
erad-dev-mx <erick_rr1@outlook.com>
2026-08-18 21:30:13 -0600

    [First try] Code of LeetCode 90

--- src/LeetCode90/Solution.java
+++ src/LeetCode90/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode90;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
@@ -8,13 +10,29 @@ class Solution {
     // [1,2,2] => [][1][2][2][1,2][1,2] => whenever we have duplicates we need to remove them
     // [][1][2][1,2]
 
-    // We will use backtracking but first we will order our array
+    // We will use backtracking, but first we will order our array
+    // Why? In order to compare if there are similar entries (handle similar)
     // [1,2,2]
     // []
     // [1] - [1,2], [1,2] THIS IS NOT VALID! - [1,2,2]
     // [2] - [2,2] -
     // [2] THIS IS NOT VALID!
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            current.add(nums[i]);
+            backtrack(result, current, nums, i + 1);
+            current.remove(current.size() - 1);
+        }
     }
 }
