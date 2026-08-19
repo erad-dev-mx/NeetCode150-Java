886be5666a6ac6673d0c3940d5dfdb3ba0fdbb84
erad-dev-mx <erick_rr1@outlook.com>
2026-08-18 21:24:13 -0600

    [First try] Thinking process of LeetCode 90

--- src/LeetCode90/Solution.java
+++ src/LeetCode90/Solution.java
@@ -1,6 +1,19 @@
 package LeetCode90;
 
+import java.util.List;
+
 class Solution {
+    // [1,2] this will contain 2 ^ n possible subsets
+    // [][1][2][1,2]
+    // [1,2,2] => [][1][2][2][1,2][1,2] => whenever we have duplicates we need to remove them
+    // [][1][2][1,2]
+
+    // We will use backtracking but first we will order our array
+    // [1,2,2]
+    // []
+    // [1] - [1,2], [1,2] THIS IS NOT VALID! - [1,2,2]
+    // [2] - [2,2] -
+    // [2] THIS IS NOT VALID!
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
     }
