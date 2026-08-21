5e34333fb90ba2bcfa3b6d950cd694fc284a929f
erad-dev-mx <erick_rr1@outlook.com>
2026-08-20 21:31:09 -0600

    [First try] Code of LeetCode 40

--- src/LeetCode40/Solution.java
+++ src/LeetCode40/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode40;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
@@ -18,8 +20,27 @@ class Solution {
     // Check all 3 combinations: ans = [1,2,2] => [[1,2,2],[1,4],[2,3]]
     // Check all 4 combinations: ans = [1,2,2] => [[1,2,2],[1,4],[2,3]]
     // In every new check we don't go to previous numbers.
-    // E.G.: When check 3 we do not check neither 1 or 2s
+    // E.G.: When check 3 we do not check neither 1 nor 2s
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
 
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] candidates, int target, int start) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            if (target - candidates[i] < 0) break;
+
+            current.add(candidates[i]);
+            backtrack(result, current, candidates, target - candidates[i], i + 1);
+            current.remove(current.size() - 1);
+        }
     }
 }
