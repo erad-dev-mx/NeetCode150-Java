d7c744c328881e750ae5bfa03f1c592ae94d236b
erad-dev-mx <erick_rr1@outlook.com>
2026-08-27 19:47:54 -0600

    [First try] Code of LeetCode 79

--- src/LeetCode79/Solution.java
+++ src/LeetCode79/Solution.java
@@ -12,7 +12,40 @@ package LeetCode79;
 // We are able to reach to thw word hence we return true.
 // Time : O(N < 3^L), Space = O(L)
 class Solution {
+    public char[][] board;
+    public int rows;
+    public int cols;
     public boolean exist(char[][] board, String word) {
-       
+        this.board = board;
+        this.rows = board.length;
+        this.cols = board[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (backtrack(r, c, word, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean backtrack(int row, int col, String word, int index) {
+        if (index >= word.length()) return true;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols || this. board[row][col] != word.charAt(index)) return false;
+
+        int[] rowDir = {0, 1, 0, -1};
+        int[] colDir = {1, 0, -1, 0};
+        this.board[row][col] = '#';
+        boolean ret = false;
+
+        for (int d = 0; d < 4; d++) {
+            ret = backtrack(row + rowDir[d], col + colDir[d], word, index + 1);
+            if (ret) break; // If we found a good neighbor
+        }
+        board[row][col] = word.charAt(index);
+        return ret;
     }
 }
