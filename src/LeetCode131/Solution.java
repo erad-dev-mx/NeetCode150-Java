251f4f1956f25cf1a9cb8f38df7df40ee74f63a6
erad-dev-mx <erick_rr1@outlook.com>
2026-08-28 20:07:49 -0600

    [First try] Code of LeetCode 131

--- src/LeetCode131/Solution.java
+++ src/LeetCode131/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode131;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // s = a,b,c
@@ -17,6 +18,31 @@ import java.util.List;
 // When we found that there is a partition that is not palindrome we stop looking down
 class Solution {
     public List<List<String>> partition(String s) {
-        
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> current, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(result, current, s, end + 1);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) return false;
+        }
+
+        return true;
     }
 }
