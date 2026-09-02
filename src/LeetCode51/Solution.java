3a33efc190220eb26b9a8f3a57eecb92656bb240
erad-dev-mx <erick_rr1@outlook.com>
2026-09-01 19:06:55 -0600

    [First try] Thinking process of LeetCode 51

--- src/LeetCode51/Solution.java
+++ src/LeetCode51/Solution.java
@@ -2,6 +2,16 @@ package LeetCode51;
 
 import java.util.List;
 
+// Basically we will receive an m x n board when x number of queens
+// A queen can move up, down, right, left and diagonal
+// We will return all posible combination in which the queens cannot attack others
+
+// We will do recursive backtracking to determine if there is a good path or not
+// First, we need to keep track of the path
+// Base case and recursion function
+// Method: check valid path
+// Method: generate the board
+// Time complexity is terrible: 2^n^2
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         
