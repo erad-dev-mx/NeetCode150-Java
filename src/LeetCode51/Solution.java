d2265cf20cfe07fae88822e3ce2416b3a4a7b9b4
erad-dev-mx <erick_rr1@outlook.com>
2026-09-01 19:19:58 -0600

    [First try] Code of LeetCode 51

--- src/LeetCode51/Solution.java
+++ src/LeetCode51/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode51;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Basically we will receive an m x n board when x number of queens
@@ -14,6 +15,60 @@ import java.util.List;
 // Time complexity is terrible: 2^n^2
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        backtrack(board, 0, res);
+        return res;
+    }
+
+    private void backtrack(char[][] board, int col, List<List<String>> res) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            if (isValid(board, i, col)) {
+                board[i][col] = 'Q';
+                backtrack(board, col + 1, res);
+                board[i][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        for (int i = 0; i < col; i++) {
+            if (board[row][i] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String row = new String(board[i]);
+            res.add(row);
+        }
+        return res;
     }
 }
