be9c14b3851e218696ea17438ee7878d6d4bdc53
erad-dev-mx <erick_rr1@outlook.com>
2026-09-07 19:39:01 -0600

    [First try] Init of LeetCode 200

---
+++ src/LeetCode133/Solution.java
@@ -0,0 +1,45 @@
+package LeetCode133;/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
