876035b8d84248c65063e3bdf2f86d188a7f5c9e
erad-dev-mx <erick_rr1@outlook.com>
2026-09-08 19:59:10 -0600

    [First try] Thinking process of LeetCode 695

--- src/LeetCode695/Solution.java
+++ src/LeetCode695/Solution.java
@@ -1,5 +1,24 @@
 package LeetCode695;
 
+// We are going to iterate in one single direction, if we found 1 we will check
+// all the neighbors, once we exhaust every single neighbor we can determine the area
+// and also check the max so far.
+
+// [0,0,1,0,0,0,0,1,0,0,0,0,0]
+// [0,0,0,0,0,0,0,1,1,1,0,0,0]
+// [0,1,1,0,1,0,0,0,0,0,0,0,0]
+// [0,1,0,0,1,1,0,0,1,0,1,0,0]
+// [0,1,0,0,1,1,0,0,1,1,1,0,0]
+// [0,0,0,0,0,0,0,0,0,0,1,0,0]
+// [0,0,0,0,0,0,0,1,1,1,0,0,0]
+// [0,0,0,0,0,0,0,1,1,0,0,0,0]
+
+// First we visited the first island (1), it is important to check as visited
+// Why? To avoid infinite loops and recount a determine value
+// Our next max area is going to be 4, then we find another 4, and so on
+
+// We are using DFS
+// Time: O(n x m), space: O(n x m)
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         
