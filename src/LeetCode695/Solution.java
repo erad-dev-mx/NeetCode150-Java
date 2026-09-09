73e272da894f833255f6e8a5e9c4b63cacd66f55
erad-dev-mx <erick_rr1@outlook.com>
2026-09-08 20:03:24 -0600

    [First try] Code of LeetCode 695

--- src/LeetCode695/Solution.java
+++ src/LeetCode695/Solution.java
@@ -21,6 +21,30 @@ package LeetCode695;
 // Time: O(n x m), space: O(n x m)
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        
+        if (grid == null || grid.length == 0) return 0;
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0; // Visited
+        int area = 1;
+        area += dfs(grid, i + 1, j);
+        area += dfs(grid, i - 1, j);
+        area += dfs(grid, i, j + 1);
+        area += dfs(grid, i, j - 1);
+        return area;
     }
 }
