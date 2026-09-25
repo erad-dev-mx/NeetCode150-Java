3f6b2558f7b995495ea894391342b0d6933e9d16
erad-dev-mx <erick_rr1@outlook.com>
2026-09-24 20:08:30 -0600

    [First try] Code of LeetCode 207

--- src/LeetCode207/Solution.java
+++ src/LeetCode207/Solution.java
@@ -1,5 +1,10 @@
 package LeetCode207;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
     // prerequisites = [[1,0],[0,3],[2,0]]
     // num = (0,1,2,3)
@@ -22,6 +27,38 @@ class Solution {
     // The additional step to validate loops? We will use a HashSet and there we will keep track of visited
     // When we try to add a duplicate item that means we are in a loop
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+        HashMap<Integer, List<Integer>> courseGraph = new HashMap<>();
+
+        for (int[] pre : prerequisites) {
+            if (courseGraph.containsKey(pre[1])) {
+                courseGraph.get(pre[1]).add(pre[0]);
+            } else {
+                List<Integer> nextCourses = new LinkedList<>();
+                nextCourses.add(pre[0]);
+                courseGraph.put(pre[1], nextCourses);
+            }
+        }
+
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (int currentCourse = 0; currentCourse < numCourses; currentCourse++) {
+            if (courseSchedule(currentCourse, visited, courseGraph) == false) return false;
+        }
+        return true;
+    }
+
+    private boolean courseSchedule(int course, HashSet<Integer> visited, HashMap<Integer, List<Integer>> courseGraph) {
+        if (visited.contains(course)) return false;
+        if (courseGraph.get(course) == null) return true;
+
+        visited.add(course);
+        for (int pre : courseGraph.get(course)) {
+            if (courseSchedule(pre, visited, courseGraph) == false) return false;
+        }
+
+        visited.remove(course);
+        courseGraph.put(course, null);
+
+        return true;
     }
 }
