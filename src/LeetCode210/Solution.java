eaadeb19235917649e4c91e400442fa26e3bd407
erad-dev-mx <erick_rr1@outlook.com>
2026-09-25 20:18:27 -0600

    [First try] Code of LeetCode 210

--- src/LeetCode210/Solution.java
+++ src/LeetCode210/Solution.java
@@ -1,5 +1,10 @@
 package LeetCode210;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
     // courses: [0, 1, 2, 3]
     // preReq: [[1,0], [2,1], [3,2]]
@@ -19,7 +24,72 @@ class Solution {
     // Find cycles? HashSet
     // Track? Stack, we will add values once we visite them, at the end should be full and the top of the stack
     // we should have a value with no preReq
+
+    // It is very important to remember this thing: in order to finish courses we don't need to do it when
+    // they are connected
+
+    static int WHITE = 1;
+    static int GRAY = 2; // During DFS it has been visited
+    static int BLACK = 3; // Is in stack
+
+    boolean isPossible;
+    Map<Integer, Integer> color;
+    Map<Integer, List<Integer>> adjList;
+    List<Integer> topologicalOrder = new ArrayList<>();
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
+        this.init(numCourses);
+
+        // Create graph
+        for (int i = 0; i < prerequisites.length; i++) {
+            int dest = prerequisites[i][0];
+            int src = prerequisites[i][1];
+            List<Integer> lst = adjList.getOrDefault(src, new ArrayList<>());
+            lst.add(dest);
+            adjList.put(src, lst);
+        }
+
+        // If the node is unprocessed then call dfs on it
+        for (int i = 0; i < numCourses; i++) {
+            if (this.color.get(i) == WHITE) this.dfs(i);
+        }
+
+        int[] order;
+        if (this.isPossible) {
+            order = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                order[i] = this.topologicalOrder.get(numCourses - i - 1);
+            }
+        } else {
+            order = new int[0];
+        }
+
+        return order;
+    }
+
+    private void init(int numCourses) {
+        this.isPossible = true;
+        this.color = new HashMap<>();
+        this.adjList = new HashMap<>();
+        this.topologicalOrder = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            this.color.put(i, WHITE);
+        }
+    }
+
+    private void dfs(int node) {
+        if (!this.isPossible) return;
+
+        this.color.put(node, GRAY);
+        for (Integer neighbor : this.adjList.getOrDefault(node, new ArrayList<>())) {
+            if (this.color.get(neighbor) == WHITE) {
+                this.dfs(neighbor);
+            } else if (this.color.get(neighbor) == GRAY) {
+                this.isPossible = false;
+            }
+        }
+
+        this.color.put(node, BLACK);
+        this.topologicalOrder.add(node);
     }
 }
